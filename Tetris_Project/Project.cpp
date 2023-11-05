bc77f518aaccdf3af223de946f10d3da39cc25ec
kye <kye6735@naver.com>
2023-11-06 08:06:04 +0900

    feat: 좌표 입력 시 해당하는 테트로미노 생성

--- Tetris_Project/Project.cpp
+++ Tetris_Project/Project.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <Windows.h>
 #include <vector>
+#include <string>
 #include <cstdlib> // rand
 #include <ctime> // time
 #include <conio.h>
+#pragma warning(disable : 4996) warning pragma
 
 using namespace std;
 
@@ -210,14 +212,25 @@ void GameTitle() {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x0007);
 }
 
-void show_graphic(std::string*** p) {
-	int i, j;
-	for (i = 0; i < 30; i++) {
-		for (j = 0; j < 30; j++) { std::cout << p[i][j]->c_str(); }
+// 편의상 구현
+void nums_to_arr(std::vector<std::vector<int>>& p, int a, int b, int c, int d, int e, int f, int g, int h) {
+	p[0][0] = a;
+	p[0][1] = b;
+	p[1][0] = c;
+	p[1][1] = d;
+	p[2][0] = e;
+	p[2][1] = f;
+	p[3][0] = g;
+	p[3][1] = h;
+}
+
+void show_graphic(std::vector<std::vector<std::string>>& p) {
+	for (int i = 0; i < 30; i++) {
+		for (int j = 0; j < 30; j++) {
+			std::cout << p[i][j];
+		}
 		std::cout << std::endl;
 	}
-
-	return;
 }
 int array(char key) {
 	switch (key) {
@@ -228,19 +241,122 @@ int array(char key) {
 	default: return -1;
 	}
 }
-void move_block(char*** p, int blocktype, int key, int x, int y) {
+
+// 블록관련
+void block_type_to_coors(std::vector<std::vector<int>>& p, int blocktype, int spinvalue) {
+	switch (blocktype) {
+	case 0:
+		switch (spinvalue) {
+		case 0:
+			nums_to_arr(p, -1, 0, 0, 0, 1, 0, 2, 0);
+			return;
+		case 1:
+			nums_to_arr(p, 0, -1, 0, 0, 0, 1, 0, 2);
+			return;
+		case 2:
+			nums_to_arr(p, -1, 0, 0, 0, 1, 0, 2, 0);
+			return;
+		case 3:
+			nums_to_arr(p, 0, -1, 0, 0, 0, 1, 0, 2);
+			return;
+		}
+	case 1:
+		switch (spinvalue) {
+		case 0:
+			nums_to_arr(p, -1, -1, 0, -1, 0, 0, 0, 1);
+			return;
+		case 1:
+			nums_to_arr(p, 0, -1, 0, 0, 0, 1, -1, 1);
+			return;
+		case 2:
+			nums_to_arr(p, -1, 0, 0, 0, 0, 1, 1, 1);
+			return;
+		case 3:
+			nums_to_arr(p, -1, 0, -1, 1, 0, 0, 1, 0);
+			return;
+		}
+	case 2:
+		switch (spinvalue) {
+		case 0:
+			nums_to_arr(p, 0, -1, 0, 0, 0, 1, -1, 1);
+			return;
+		case 1:
+			nums_to_arr(p, -1, -1, -1, 0, 0, 0, 1, 0);
+			return;
+		case 2:
+			nums_to_arr(p, 1, -1, 0, -1, 0, 0, 0, 1);
+			return;
+		case 3:
+			nums_to_arr(p, -1, 0, 0, 0, 0, 1, 1, 1);
+			return;
+		}
+	case 3:
+		switch (spinvalue) {
+		case 0:
+			nums_to_arr(p, -1, 0, 0, 0, 0, 1, 1, 1);
+			return;
+		case 1:
+			nums_to_arr(p, 0, 0, 0, 1, 1, -1, 1, 0);
+			return;
+		case 2:
+			nums_to_arr(p, -1, 0, 0, 0, 0, 1, 1, 1);
+			return;
+		case 3:
+			nums_to_arr(p, 0, 0, 0, 1, 1, -1, 1, 0);
+			return;
+		}
+	case 4:
+		switch (spinvalue) {
+		case 0:
+			nums_to_arr(p, 0, 0, 0, 1, -1, 1, 1, 0);
+			return;
+		case 1:
+			nums_to_arr(p, -1, -1, -1, 0, 0, 0, 0, 1);
+			return;
+		case 2:
+			nums_to_arr(p, 0, 0, 0, 1, -1, 1, 1, 0);
+			return;
+		case 3:
+			nums_to_arr(p, -1, -1, -1, 0, 0, 0, 0, 1);
+			return;
+		}
+	case 5:
+		switch (spinvalue) {
+		case 0:
+			nums_to_arr(p, 0, 0, 0, 1, 1, 0, 1, 1);
+			return;
+		case 1:
+			nums_to_arr(p, 0, 0, 0, 1, 1, 0, 1, 1);
+			return;
+		case 2:
+			nums_to_arr(p, 0, 0, 0, 1, 1, 0, 1, 1);
+			return;
+		case 3:
+			nums_to_arr(p, 0, 0, 0, 1, 1, 0, 1, 1);
+			return;
+		}
+	}
+}
+void put_block(std::vector<std::vector<std::string>>& p, std::vector<std::vector<int>>& q, int a, int b) {
+	for (int i = 0; i < 4; i++) {
+		int tenpi = a + q[i][0];
+		int tenpj = b + q[i][1];
+		p[tenpi][tenpj] = "■";
+	}
+}
+
+void move_block(std::vector<std::vector<std::string>>& p, int blocktype, int key, int x, int y) {
 
 }
-void show_blockpoints(char** p) {
-	int i, j;
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 2; j++) {
-			printf(" %d", p[i][j]);
+void show_blockpoints(std::vector<std::vector<int>>& p) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 2; j++) {
+			std::cout << " " << p[i][j];
 		}
-		printf("\n");
+		std::cout << std::endl;
 	}
-	return;
 }
+
 
 
 // 테트리스 화면 크기 구상 : 대부분 10*20 사용
@@ -249,51 +365,44 @@ int main() {
 	// GameTitle();
 
 	// ▩□■
-	int i = 0, j = 0;
-	std::string*** graphic; // 문자열을 원소로 갖는 2차원 배열
-	int** block_points;
+	int testi, testj;
+	std::vector<std::vector<std::string>> graphic(30, std::vector<std::string>(30)); // 문자열을 원소로 갖는 2차원 배열
+	std::vector<std::vector<int>> block_coors(4, std::vector<int>(2));
 
-	// graphic 동적할당
-	graphic = new std::string **[30]; // 30개의 행
-	for (i = 0; i < 30; i++) {
-		graphic[i] = new std::string * [30]; // 30개의 열
-	}
-
-	block_points = (int**)malloc(sizeof(int*) * 4);
-	for (i = 0; i < 4; i++) block_points[i] = (int*)malloc(sizeof(int) * 2);
-
-	//show_blockpoints(block_points);
-
-	// 기본세팅
-	for (i = 0; i < 30; i++) {
-		for (j = 0; j < 30; j++) {
-			if (i == 4 && j > 8 && j < 21) {
-				graphic[i][j] = new std::string("▩");
-			}
-			else if (i == 25 && j > 8 && j < 21) {
-				graphic[i][j] = new std::string("▩");
-			}
-			else if (j == 9 && i > 3 && i < 26) {
-				graphic[i][j] = new std::string("▩");
-			}
-			else if (j == 20 && i > 3 && i < 26) {
-				graphic[i][j] = new std::string("▩");
-			}
-			else graphic[i][j] = new std::string("  ");
-
+	// 기본 설정
+	for (int i = 0; i < 30; i++) {
+		for (int j = 0; j < 30; j++) {
+			if (i == 4 && j > 8 && j < 21) graphic[i][j] = "▩";
+			else if (i == 25 && j > 8 && j < 21) graphic[i][j] = "▩";
+			else if (j == 9 && i > 3 && i < 26) graphic[i][j] = "▩";
+			else if (j == 20 && i > 3 && i < 26) graphic[i][j] = "▩";
+			else graphic[i][j] = "  ";
 		}
 	}
 
-	show_graphic(graphic);
+	while (1) {
+		// 게임 루프
+		// 블록 이동 및 게임 로직 구현 필요
+		std::cin >> testi >> testj;
+		if (testi == -1) return 0;
+		block_type_to_coors(block_coors, testi, testj);
+		put_block(graphic, block_coors, 15, 15);
+		show_graphic(graphic);
+		// 블록 이동 로직 호출
+		// ...
 
-	// 할당된 메모리 해제
-	for (i = 0; i < 30; i++) {
-		for (j = 0; j < 30; j++) {
-			delete graphic[i][j];
+		// 그래픽 초기화
+		for (int i = 0; i < 30; i++) {
+			for (int j = 0; j < 30; j++) {
+				if (i == 4 && j > 8 && j < 21) graphic[i][j] = "▩";
+				else if (i == 25 && j > 8 and j < 21) graphic[i][j] = "▩";
+				else if (j == 9 && i > 3 and i < 26) graphic[i][j] = "▩";
+				else if (j == 20 && i > 3 and i < 26) graphic[i][j] = "▩";
+				else graphic[i][j] = "  ";
+			}
 		}
-		delete[] graphic[i];
 	}
-	delete[] graphic;
 
+	// 할당된 메모리 해제 (std::vector는 자동으로 관리)
 	return 0;
 }
